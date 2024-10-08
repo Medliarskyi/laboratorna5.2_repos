612b00659ab82bebed5da206ed61bfceba612507
Medliarskyi <med.uras111@gmail.com>
2024-10-08 21:47:06 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab52/lab52.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double result = A(0.5, 2, 1.0);
+			Assert::AreEqual(result, -2.4, 0.001);
+		}
+	};
+}
---
+++ lab52/lab52.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+#define M_PI 3.14159265358979323846  // Визначення константи π
+
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+
+int main()
+{
+    double xp, xk, x, dx, eps, s = 0;
+    int n = 0;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+    cout << fixed;
+    cout << "-----------------------------------------" << endl;
+    cout << "|" << setw(5) << "x" << "   |"
+        << setw(10) << "arctg(x)" << " |"
+        << setw(7) << "S" << "    |"
+        << setw(5) << "n" << " |"
+        << endl;
+    cout << "-----------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        s = S(x, eps, n, s) + M_PI / 2;  // Додаємо π/2 до кожного значення s
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << atan(x) << " |"
+            << setw(10) << setprecision(5) << s << " |"
+            << setw(5) << n << " |"
+            << endl;
+        x += dx;
+    }
+    cout << "-----------------------------------------" << endl;
+
+    return 0;
+}
+
+double S(const double x, const double eps, int& n, double s)
+{
+    n = 0;
+    double a = -1 / x;
+    s = a;
+    do {
+        n++;
+        a = A(x, n, a);
+        s += a;
+    } while (abs(a) >= eps);
+    return s;
+}
+
+double A(const double x, const int n, double a)
+{
+    double R = -(2. * n - 1) / (2 * n + 1) * (1 / pow(x, 2));
+    a *= R;
+    return a;
+}
